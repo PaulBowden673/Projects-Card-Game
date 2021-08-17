f073d10ca01df4d30fa19708aa67e81bd98b9444
Paul Bowden <paulbowden673@gmail.com>
2021-08-17 10:12:16 +0000

    added hard-game variables

--- assets/js/hard-game.js
+++ assets/js/hard-game.js
@@ -4,3 +4,10 @@ document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('timer-and-home').classList.add('no-display');
     document.getElementById('board-content').classList.add('no-display');
 });
+// Variables for hard-game
+const hardGameGrid = document.querySelector('#board');
+var hardCardsSelected = [];
+var hardCardsSelectedId = [];
+var hardCardsRight = [];
+
+document.getElementById('hard-button').addEventListener('click', startHardGame);
