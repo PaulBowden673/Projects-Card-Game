32f86e9e55f4d1c470c15d14a6e76295f145e1bb
Paul Bowden <paulbowden673@gmail.com>
2021-08-17 10:45:20 +0000

    added timer function

--- assets/js/easy-game.js
+++ assets/js/easy-game.js
@@ -10,3 +10,21 @@ function displayGame() {
     document.getElementById('board-content').classList.remove('no-display');
     setTimer();
 }
+// Credit for timer functions: user efuzz on Stack Overflow.
+var totalSeconds = 0;
+
+function setTimer(){
+    ++totalSeconds;
+    var seconds = document.getElementById('seconds');
+    seconds.innerHTML = pad(totalSeconds%60);
+    var minutes = document.getElementById('minutes');
+    minutes.innerHTML = pad(parseInt(totalSeconds/60));
+}
+
+function pad(val){
+    var valString = val + '';
+    if(valString.length < 2) {
+        return '0' + valString;
+    } else { return valString;
+    }
+}
