30fa7e4d2fe3caef34d2a4e76125a2aa816c68cd
Paul Bowden <paulbowden673@gmail.com>
2021-08-17 11:05:57 +0000

    added start function for medium game

--- assets/js/medium-game.js
+++ assets/js/medium-game.js
@@ -3,3 +3,15 @@ const medGameGrid = document.querySelector('#board');
 var medCardsSelected = [];
 var medCardsSelectedId = [];
 var medCardsRight = [];document.getElementById('medium-button').addEventListener('click', startMediumGame);
+
+// Start funcrion for medium-game
+function startMediumGame() {
+    displayGame();
+    createModBoard();
+    document.getElementById('reset').addEventListener('click', resetMedGame);
+    counter.innerHTML = `0`;
+    resultDisplay.innerHTML = `0`;
+ 
+}
+
+const kittenCardsModerate = kittenCardsHard.slice(0,12);
