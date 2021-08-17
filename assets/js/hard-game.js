7b011fe81a462364cca79cbebe6effa3688ff748
Paul Bowden <paulbowden673@gmail.com>
2021-08-17 10:19:24 +0000

    added game objects

--- assets/js/hard-game.js
+++ assets/js/hard-game.js
@@ -11,3 +11,33 @@ var hardCardsSelectedId = [];
 var hardCardsRight = [];
 
 document.getElementById('hard-button').addEventListener('click', startHardGame);
+
+// start up function for hard-game
+function startHardGame() {
+    displayGame();
+    createHardBoard();
+    document.getElementById('reset').addEventListener('click', resetHardGame);
+    counter.innerHTML = `0`;
+    resultDisplay.innerHTML = `0`;
+}
+// Array of objects to be injected into the DOM used with .slice method for all games
+const kittenCardsHard = [
+    { name: 'kitten1', img: './assets/images/kitten1.png', },
+    { name: 'kitten1', img: './assets/images/kitten1.png', },
+    { name: 'kitten2', img: './assets/images/kitten2.png', },
+    { name: 'kitten2', img: './assets/images/kitten2.png', },
+    { name: 'kitten3', img: './assets/images/kitten3.png', },
+    { name: 'kitten3', img: './assets/images/kitten3.png', },
+    { name: 'kitten4', img: './assets/images/kitten4.png', },
+    { name: 'kitten4', img: './assets/images/kitten4.png', },
+    { name: 'kitten5', img: './assets/images/kitten5.png', },
+    { name: 'kitten5', img: './assets/images/kitten5.png', },
+    { name: 'kitten6', img: './assets/images/kitten6.png', },
+    { name: 'kitten6', img: './assets/images/kitten6.png', },
+    { name: 'kitten7', img: './assets/images/kitten7.png', },
+    { name: 'kitten7', img: './assets/images/kitten7.png', },
+    { name: 'kitten8', img: './assets/images/kitten8.png', },
+    { name: 'kitten8', img: './assets/images/kitten8.png', },
+    { name: 'kitten9', img: './assets/images/kitten9.png', },
+    { name: 'kitten9', img: './assets/images/kitten9.png', },
+];
