4d78cc87a0f0e59ca9721ffd50e8d2aa95124a85
Paul Bowden <paulbowden673@gmail.com>
2021-08-17 10:51:06 +0000

    game board for easy-game added

--- assets/js/easy-game.js
+++ assets/js/easy-game.js
@@ -28,3 +28,41 @@ function pad(val){
     } else { return valString;
     }
 }
+
+// Variables for easy-game
+const easyGameGrid = document.querySelector('#board');
+const counter = document.querySelector('#count');
+var easyCardsSelected = [];
+var easyCardsSelectedId = [];
+var easyCardsRight = [];
+
+document.getElementById('easy-button').addEventListener('click', startEasyGame);
+
+function startEasyGame() {
+    displayGame();
+    createEasyBoard();
+    document.getElementById('reset').addEventListener('click', resetEasyGame);
+    counter.innerHTML = `0`;
+    resultDisplay.innerHTML = `0`;
+    
+}
+
+const kittenCardsEasy = kittenCardsMedium.slice(0, 6);
+
+// Credit for function: Ania Kubow you-tube
+function createEasyBoard() {
+    // Credit for .sort method of shuffling: Marina Ferreira
+    kittenCardsEasy.sort(() => 0.5 - Math.random());
+    // game board id changed to fit in browser
+    easyGameGrid.id = 'easyboard';
+    for (let i = 0; i < kittenCardsEasy.length; i++) {
+        var easyCard = document.createElement('img');
+        easyCard.setAttribute('src', './assets/images/kitten-card-back.png');
+        easyCard.setAttribute('data-id', i);
+        // Add alt text to make sure screen reader users can also play the game
+        easyCard.setAttribute('alt', 'Card back, select to flip over');
+        easyCard.classList.add('col-6', 'col-lg-4', 'kittenCard');
+        easyCard.addEventListener('click', flipEasyCard);
+        easyGameGrid.appendChild(easyCard);
+    }
+}
