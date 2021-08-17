48ff3278dd9600372494172634e654f39ee14b30
Paul Bowden <paulbowden673@gmail.com>
2021-08-17 10:09:27 +0000

    added js to hide game until start

---
+++ assets/js/hard-game.js
@@ -0,0 +1,6 @@
+// Hides game until game difficulty level is selected
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('board').classList.add('no-display');
+    document.getElementById('timer-and-home').classList.add('no-display');
+    document.getElementById('board-content').classList.add('no-display');
+});
