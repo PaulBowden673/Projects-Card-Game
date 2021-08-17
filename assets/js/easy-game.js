9671b35052323e2bb706e2ae108f80bd691fa352
Paul Bowden <paulbowden673@gmail.com>
2021-08-17 10:43:52 +0000

    added function to insert game and hide instructions

--- assets/js/easy-game.js
+++ assets/js/easy-game.js
@@ -1,2 +1,12 @@
 // Global variable score counter
 const resultDisplay = document.querySelector('#result');
+
+// Removes how to play information inserts game
+function displayGame() {
+    document.getElementById('board').classList.remove('no-display');
+    document.getElementById('timer-and-home').classList.remove('no-display');
+    document.getElementById('introduction-section').remove();
+    document.getElementById('start-game-prompt').remove();
+    document.getElementById('board-content').classList.remove('no-display');
+    setTimer();
+}
